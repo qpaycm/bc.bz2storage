521ac556fcc03f801ef1df8fab2595fbd5cdc68b
Al Po <ineed4@gmail.com>
2019-07-03 12:29:16 +0300

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -11,15 +11,12 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-import os
-import pathlib
-from setuptools import setup, find_packages
+name, version = 'zc.bz2storage', '1.0.1'
 
 install_requires = [
     'setuptools',
     'ZODB',
     'zope.interface',
-	'bz2file'
 ]
 extras_require = {
     'test': [
@@ -32,49 +29,44 @@ extras_require = {
 entry_points = """
 """
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-# The text of the README file
-README = (HERE / "README.txt").read_text()
-name, version = 'zc.bz2storage', '1.0.11'
-description="Compression improvement for ZODB",
-long_description=README,
-long_description_content_type="text/markdown",
-url="https://github.com/qpaycm/zc.bz2storage",
+from setuptools import setup
+import os
+
+
+def read(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+readme = read(os.path.join('src', *name.split('.') + ['README.txt']))
+changes = read('CHANGES.rst')
+long_description = readme + '\n\n' + changes
 
 setup(
     author='vir2alexport',
     author_email='vir2alexport@gmail.com',
     license='ZPL 2.1',
+
     name=name, version=version,
+    long_description=long_description,
+    description=long_description.split('\n')[1],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Zope Public License",
-        "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        "Framework :: Zope3",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Software Development :: Testing",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     packages=[name.split('.')[0], name],
-	namespace_packages=[name.split('.')[0]],
-	package_dir={'zc.bz2storage':'zc\\bz2storage'},
+    namespace_packages=[name.split('.')[0]],
+    package_dir={'': 'src'},
     install_requires=install_requires,
     zip_safe=False,
     entry_points=entry_points,
     include_package_data=True,
     extras_require=extras_require,
-	tests_require=extras_require['test'],
+    tests_require=extras_require['test'],
     test_suite=name + '.tests.test_suite',
 )
